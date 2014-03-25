f9b9ae9b3a032d0f415a124ef9420e8cf5a44cac
Petr Ablyazov <phoenix-h@mail.ru>
2014-03-25 14:06:25 +0400

    Add methtods member, Init, reset

--- lib/calc.rb
+++ lib/calc.rb
@@ -5,22 +5,34 @@ module Calc
 
 	end
 
-	  def self.sum(par1, par2)
-	  		par1 + par2
+	  def self.Init(memb = 0)
+	  		@sost = memb
 	  end
 
-	  def self.minus(par1, par2)
-	  		par1 - par2
+	  def self.sum(par1, par2 = @sost)
+	  		@sost = par1 + par2
 	  end
 
-	  def self.mul(par1, par2)
-	  		par1 * par2
+	  def self.minus(par1, par2 = @sost)
+	  		@sost = par1 - par2
 	  end
 
-	  def self.del(par1, par2)
-	  		par1 / par2
+	  def self.mul(par1, par2 = @sost)
+	  		@sost = par1 * par2
+	  end
+
+	  def self.del(par1, par2 = @sost)
+	  		@sost = par1 / par2
 	  	rescue ZeroDivisionError => e 
 	  		raise CalcZeroDivisionError
+	  end
+
+	  def self.memb(par1)
+	  		@sost = par1
+	  end
+
+	  def self.reset
+	  		@sost = 0
 	  end
 
 end
--- test/lib/calc_test.rb
+++ test/lib/calc_test.rb
@@ -1,10 +1,21 @@
 require "test_helper"
 class CalcTest < TestCase
+	
+	def test_init
+		result = Calc.Init
+		assert { result == 0 }
+		result = Calc.Init(7)
+		assert { result == 7 }
+	end
+	
 	def test_sum
 		result = Calc.sum(2,3)
 		assert { result == 5 }
-		result = Calc.sum(3,4)
-		assert { result == 7 }
+		result = Calc.sum(3)
+		assert { result == 8 }
+		result = Calc.sum(4)
+		assert { result == 12 }
+
 	end
 
 	def test_minus
@@ -12,6 +23,8 @@ class CalcTest < TestCase
 		assert { result == -1 }
 		result = Calc.minus(6,4)
 		assert { result == 2 }
+		result = Calc.minus(3)
+		assert { result == 1 }
 	end
 
 	def test_mul
@@ -29,4 +42,21 @@ class CalcTest < TestCase
 		assert_raises(Calc::CalcZeroDivisionError){ Calc.del(3,0) }
 	end
 
+	def test_memb
+		memb = Calc.memb(0)
+		assert { memb == 0 }
+		memb = Calc.memb(7)
+		assert { memb == 7 }
+		result = Calc.mul(2)
+		assert { result == 14 }
+		result = Calc.mul(2)
+		assert { result == 28 }
+		assert { memb == 7 }
+	end
+
+	def test_reset
+		result = Calc.reset
+		assert { result == 0 }
+
+	end
 end
